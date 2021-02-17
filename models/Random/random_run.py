c7861069e18a0eec56012e6d13d11c556fd14bf8
rasad <rayza.asad@gmail.com>
2021-02-17 14:02:41 -0800

    corrected the issues with the random baselines

--- models/Random/random_run.py
+++ models/Random/random_run.py
@@ -19,14 +19,16 @@ def map_cats_to_scene_objects(df):
     return cat_to_scene_objects
 
 
-def map_cat_to_objects(scene):
+def map_cat_to_objects(scene, source_node):
     cat_to_objects = {}
+    # map each category to its object id except for the source node
     for obj, node_info in scene.items():
-        cat = node_info['category'][0]
-        if cat not in cat_to_objects:
-            cat_to_objects[cat] = [obj]
-        else:
-            cat_to_objects[cat].append(obj)
+        if obj != source_node:
+            cat = node_info['category'][0]
+            if cat not in cat_to_objects:
+                cat_to_objects[cat] = [obj]
+            else:
+                cat_to_objects[cat].append(obj)
     return cat_to_objects
 
 
@@ -55,8 +57,12 @@ def Random(query_info, model_names, scene_graph_dir, mode, topk):
             sample_context = np.random.choice(all_except_target, num_context_objects, replace=False).tolist()
         else:
             sample_context = all_except_target
-        subscene = {'scene_name': scene_name, 'target': target, 'context_objects': sample_context}
+        subscene = {'scene_name': scene_name, 'target': target, 'context_objects': sample_context,
+                    'context_match': len(sample_context)}
         target_subscenes.append(subscene)
+
+    # sort the target subscenes based on the number of matching context objects
+    target_subscenes = sorted(target_subscenes, reverse=True, key=lambda x: x['context_match'])
 
     return target_subscenes
 
@@ -86,7 +92,7 @@ def CategoryRandom(query_info, query_mode, scene_graph_dir, mode, cat_to_scene_o
 
         # load the scene and map its cats to the objects
         scene = load_from_json(os.path.join(scene_graph_dir, mode, scene_name))
-        cat_to_objects = map_cat_to_objects(scene)
+        cat_to_objects = map_cat_to_objects(scene, target_object)
 
         # for each context object in the query scene sample an object with the same category from the current scene.
         for cat, freq in context_cat_to_freq.items():
@@ -108,19 +114,24 @@ def CategoryRandom(query_info, query_mode, scene_graph_dir, mode, cat_to_scene_o
 def main():
     # define initial parameters
     mode = 'val'
-    model_name = 'CategoryRandom'
+    model_name = 'Random'
     experiment_name = 'base'
     query_dict_input_path = '../../queries/matterport3d/query_dict_{}.json'.format(mode)
     query_dict_output_path = '../../results/matterport3d/{}/query_dict_{}_{}.json'.format(model_name,
                                                                                           mode,
                                                                                           experiment_name)
     scene_graph_dir = '../../data/matterport3d/scene_graphs'
+    accepted_cats = set(load_from_json('../../data/matterport3d/accepted_cats.json'))
     topk = 200
 
     # read the query dict and the metadata
     query_dict = load_from_json(query_dict_input_path)
     df_metadata = pd.read_csv('../../data/matterport3d/metadata.csv')
     df_metadata = df_metadata[df_metadata['split'] == mode]
+
+    # filter the metadata to only include objects with accepted category
+    cat_is_accepted = df_metadata['mpcat40'].apply(lambda x: x in accepted_cats)
+    df_metadata = df_metadata.loc[cat_is_accepted]
 
     # map the categories to scene objects if necessary
     if model_name == 'Random':
@@ -139,6 +150,14 @@ def main():
 
     else:
         raise Exception('Model not defined')
+
+    # test if for each query the query node is not in the context nodes
+    for query, query_info in query_dict.items():
+        for target_subscene in query_info['target_subscenes']:
+            t = target_subscene['target']
+            context_objects = target_subscene['context_objects']
+            if t in context_objects:
+                raise Exception('Target node was included in the context objects.')
 
     # save the changes to query dict
     write_to_json(query_dict, query_dict_output_path)
