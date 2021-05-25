02152d627094900bcc4834ee75ebf65737b0782f
rasad <rayza.asad@gmail.com>
2021-05-25 16:43:24 -0700

    Added a script for extracting the scene information.

--- scripts/build_scenes_matterport.py
+++ scripts/build_scenes_matterport.py
@@ -4,7 +4,7 @@ import numpy as np
 from time import time
 
 from base_scene import BaseScene
-from helper import write_to_json, create_train_val_test
+from scripts.helper import write_to_json, create_train_val_test
 
 
 def process_scenes(scene_files, metadata_path, models_dir, scene_graph_dir):
@@ -26,9 +26,6 @@ def process_scenes(scene_files, metadata_path, models_dir, scene_graph_dir):
         scene = BaseScene(models_dir)
         scene.build_from_matterport(scene_name, metadata_path)
 
-        # visualize the scenes
-        # scene.visualize()
-
         # save the scene recipe
         scene_graph_path = os.path.join(scene_graph_dir, scene_name+'.json')
         if len(scene.graph) > 0:
@@ -42,26 +39,24 @@ def process_scenes(scene_files, metadata_path, models_dir, scene_graph_dir):
     print('Processing {} scenes took {} minutes'.format(idx, round(duration_all, 2)))
 
 
-def main(num_chunks, chunk_idx):
-    build_scenes = False
-    split_train_test_val = False
-
+def main(num_chunks, chunk_idx, action='build_scenes'):
     # define the paths
     room_dir = '../data/matterport3d/rooms'
     scene_files = os.listdir(room_dir)
-    # scene_files = ['zsNo4HB9uLZ_room20']
     metadata_path = '../data/matterport3d/metadata.csv'
     models_dir = '../data/matterport3d/models'
     scene_graph_dir = '../data/matterport3d/scene_graphs/all'
+    if not os.path.exists(scene_graph_dir):
+        os.makedirs(scene_graph_dir)
 
-    if build_scenes:
+    if action == 'build_scenes':
         # process the scenes in batches
         chunk_size = int(np.ceil(len(scene_files) / num_chunks))
         process_scenes(scene_files[chunk_idx * chunk_size: (chunk_idx + 1) * chunk_size],
                        metadata_path,
                        models_dir,
                        scene_graph_dir)
-    if split_train_test_val:
+    if action == 'split_train_test_val':
         data_dir = '../data/matterport3d'
         scene_graph_dir = '../data/matterport3d/scene_graphs'
         train_path = os.path.join(data_dir, 'scenes_train.txt')
@@ -72,8 +67,10 @@ def main(num_chunks, chunk_idx):
 
 if __name__ == '__main__':
     if len(sys.argv) == 1:
-        main(1, 0)
+        main(1, 0, 'build_scenes')
+    elif len(sys.argv) == 2:
+        main(1, 0, sys.argv[1])
     else:
         # To run in parallel you can use the command:
-        # parallel -j5 "python3 -u build_scenes_matterport.py main {1} {2}" > test_logs.txt ::: 5 ::: 0 1 2 3 4
-        main(int(sys.argv[2]), int(sys.argv[3]))
+        # parallel -j5 "python3 -u build_scenes_matterport.py {1} {2} {3}" ::: 5 ::: 0 1 2 3 4 ::: build_scenes
+        main(int(sys.argv[1]), int(sys.argv[2]), sys.argv[3])
