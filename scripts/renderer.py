bd77edbea8dd94e4a8cce6b305a55132f93e7b07
rasad <rayza.asad@gmail.com>
2021-05-28 14:51:08 -0700

    Added the renderer.

--- scripts/renderer.py
+++ scripts/renderer.py
@@ -1,5 +1,3 @@
-import trimesh
-from PIL import Image
 import numpy as np
 import pyrender
 from pyrender import RenderFlags
@@ -10,49 +8,6 @@ class Render:
     def __init__(self, kwargs, camera_pose=None):
         self.kwargs = kwargs
         self.camera_pose = camera_pose
-
-    @staticmethod
-    def trimesh_render(scene, resolution, offscreen=True):
-        if not offscreen:
-            scene.show()
-        img = scene.save_image(resolution=resolution, visible=True)
-        img = Image.open(trimesh.util.wrap_as_stream(img))
-        img = np.asarray(img, dtype=np.uint8)
-        return img
-
-    def center_view_render(self, scene, resolution, camera_pose, room_dimension):
-        length, width, height = room_dimension
-        # convert trimesh scene to pyrender scene
-        scene = pyrender.Scene.from_trimesh_scene(scene)
-
-        # create and adjust one point light source and one directional one.
-        light_directional = pyrender.DirectionalLight(color=[255.0, 255.0, 255.0],
-                                                      intensity=self.kwargs['light_directional_intensity'])
-        light_directional_pose = camera_pose.copy()
-        light_directional_pose[2, 3] = height - self.kwargs['wall_thickness']
-
-        light_point_spot_center = pyrender.SpotLight(color=[255.0, 255.0, 255.0],
-                                                     intensity=self.kwargs['light_point_intensity_center'],
-                                                     innerConeAngle=np.pi/2, outerConeAngle=np.pi/2)
-        light_point_spot_pose_center = camera_pose.copy()
-        light_point_spot_pose_center[2, 3] = height - self.kwargs['wall_thickness']
-
-        # create and adjust camera
-        camera = pyrender.PerspectiveCamera(yfov=self.kwargs['fov'], aspectRatio=1)
-
-        # add light and camera to the scene
-        scene.add(light_directional, pose=light_directional_pose)
-        scene.add(light_point_spot_center, pose=light_point_spot_pose_center)
-        scene.add(camera, pose=camera_pose)
-
-        # render the image
-        flags = RenderFlags.ALL_SOLID
-        r = pyrender.OffscreenRenderer(viewport_width=resolution[0], viewport_height=resolution[1], point_size=1.0)
-        img, depth = r.render(scene, flags)
-        r.delete()
-        del scene
-        gc.collect()
-        return img
 
     def pyrender_render(self, scene, resolution, camera_pose, room_dimension):
         length, width, height = room_dimension
@@ -71,26 +26,21 @@ class Render:
         light_point_spot_pose_center = camera_pose.copy()
         light_point_spot_pose_center[2, 3] = height - self.kwargs['wall_thickness']
 
-        # ensure the entire room is in the field of view of camera.
-        # if abs(length - width) > 2:
-        #     camera_pose[2, 3] = np.max([(length / 2) / (np.tan(self.kwargs['fov'] / 2)),
-        #                                 (width / 2) / np.tan(self.kwargs['fov'] / 2)]) + height
-        # else:
         max_dim = np.maximum(length, width)
         x = np.sqrt(max_dim**2 + max_dim**2)
         camera_pose[2, 3] = np.maximum(x / (2 * np.tan(self.kwargs['fov'] / 2)) + height,
                                        camera_pose[2, 3] + height)
 
         # ensure the near plane of camera passes the ceiling
-        # z_near = camera_pose[2, 3] - height - self.kwargs['wall_thickness']
-        # i = 1
-        # while z_near < 0:
-        #     z_near = camera_pose[2, 3] - height - self.kwargs['wall_thickness'] / 2**i
-        #     i += 1
+        z_near = camera_pose[2, 3] - height - self.kwargs['wall_thickness']
+        i = 1
+        while z_near < 0:
+            z_near = camera_pose[2, 3] - height - self.kwargs['wall_thickness'] / 2**i
+            i += 1
 
         # create and adjust camera
         camera = pyrender.PerspectiveCamera(yfov=self.kwargs['fov'], aspectRatio=1)
-        # camera.znear = z_near
+        camera.znear = z_near
 
         # add light and camera to the scene
         scene.add(light_directional, pose=light_directional_pose)
