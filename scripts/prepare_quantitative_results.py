926c007640086ff9f04b7fbdb9bc810364fa9060
rasad <rayza.asad@gmail.com>
2021-05-29 13:43:49 -0700

    Added script for displaying the evaluation plots and the area under the curves.

--- scripts/prepare_quantitative_results.py
+++ scripts/prepare_quantitative_results.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 from sklearn.metrics import auc
 from time import time
+from optparse import OptionParser
 from matplotlib import pyplot as plt
 
 
@@ -16,7 +17,7 @@ def plot_results(metrics, model_name_exp_map, summary_results, plot_name, with_a
             summary_resutls_model = summary_results.loc[summary_results['model_name'] == model_name]
             # choose the experiment under the model name
             this_experiment = summary_resutls_model['experiment_id'].apply(
-                lambda x: x.split('-')[0] == experiment_id)
+                lambda x: x.split('-0')[0] == experiment_id)
             summary_resutls_model_experiment = summary_resutls_model.loc[this_experiment]
             # x axis represents thresholds
             x = summary_resutls_model_experiment['experiment_id'].apply(lambda x: np.float(x.split('-')[-1])).values
@@ -37,30 +38,46 @@ def plot_results(metrics, model_name_exp_map, summary_results, plot_name, with_a
         print(auc_dic)
 
 
+def get_args():
+    parser = OptionParser()
+    parser.add_option('--mode', dest='mode', default='test', help='val|test')
+    parser.add_option('--ablations', action='store_true', dest='ablations', default=True,
+                      help='If True the ablation results are plotted.')
+
+    (options, args) = parser.parse_args()
+    return options
+
+
 def main():
+    # load the arguments
+    args = get_args()
+
     # define the paths and params
-    mode = 'test'
-    aggregated_csv_path = '../results/matterport3d/evaluations/{}/evaluation_aggregated.csv'.format(mode)
+    if args.ablations:
+        aggregated_csv_path = '../results/matterport3d/evaluations/ablation/evaluation_aggregated.csv'
+    else:
+        aggregated_csv_path = '../results/matterport3d/evaluations/{}/evaluation_aggregated.csv'.format(args.mode)
 
     # read the aggregated results and choose the metric to plot
     summary_results = pd.read_csv(aggregated_csv_path)
     metrics = ['overlap_mAP']
 
     # define the models that you would like to plot and the name that you will display
-    # model_name_exp_map = {('LearningBased', 'lstm_with_cats'): 'AlignRankOracle',
-    #                       ('LearningBased', 'lstm_top1_predictions'): 'AlignRank',
-    #                       ('GK++', 'base'): 'GKRank',
-    #                       ('CategoryRandom', 'base'): 'CatRank',
-    #                       ('LearningBased', 'lstm_top1_predictions_kmeans'): 'AlignRankKmeans',
-    #                       ('Random', 'base'): 'Random'}
+    if args.ablations:
+        model_name_exp_map = {('LearningBased', 'AlignRank'): 'AlignRank',
+                              ('LearningBased', 'AlignRank[-GNN]'): 'AlignRank[-GNN]',
+                              ('LearningBased', 'AlignRank[-Align]'): 'AlignRank[-Align]',
+                              ('SVDRank', 'SVDRank1D'): 'SVDRank1D',
+                              ('SVDRank', 'SVDRank3D'): 'SVDRank3D'}
+        plot_name = 'alignment_ablation_global.png'
 
-    model_name_exp_map = {('LearningBased', 'lstm_top1_predictions'): 'AlignRank',
-                          ('LearningBased', 'lstm_top1_predictions_linear_classifier'): 'AlignRank[-GNN]',
-                          ('LearningBased', 'no_alignment'): 'AlignRank[-Align]',
-                          ('SVDRank', 'with_cat_predictions_3d'): 'SVDRank3D',
-                          ('SVDRank', 'with_cat_predictions_1d'): 'SVDRank1D'}
-
-    plot_name = 'alignment_ablation_global.png'.format(mode)
+    else:
+        model_name_exp_map = {('LearningBased', 'AlignRankOracle'): 'AlignRankOracle',
+                              ('LearningBased', 'AlignRank'): 'AlignRank',
+                              ('GKRank', 'GKRank'): 'GKRank',
+                              ('CatRank', 'CatRank'): 'CatRank',
+                              ('RandomRank', 'RandomRank'): 'RandomRank'}
+        plot_name = 'mAP_comparisons.png'
 
     # plot the quantiative results
     plot_results(metrics, model_name_exp_map, summary_results, plot_name, with_auc=True)
