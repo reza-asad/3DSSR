19dae71d155e29bc7260d428506f741ed14a1378
rasad <rayza.asad@gmail.com>
2021-06-01 17:55:59 -0700

    Filtering scene graphs by accepted cats.

--- models/GKRank/build_scene_graphs_matterport.py
+++ models/GKRank/build_scene_graphs_matterport.py
@@ -4,11 +4,11 @@ import numpy as np
 from time import time
 
 from scene_graphs import SceneGraph
-from helper import create_train_val_test
+from helper import create_train_val_test, load_from_json
 
 
-def process_scenes(scene_files, models_dir, metadata_path, scene_graph_dir, test_objects, num_samples, dist_eps,
-                   angle_eps):
+def process_scenes(scene_files, models_dir, metadata_path, accepted_cats, scene_graph_dir, test_objects, num_samples,
+                   dist_eps, angle_eps):
     t = time()
     idx = 0
     for scene_name in scene_files:
@@ -24,15 +24,19 @@ def process_scenes(scene_files, models_dir, metadata_path, scene_graph_dir, test
             continue
 
         # first initialize the graph
-        scene_graph = SceneGraph(models_dir, scene_graph_dir, scene_name)
+        scene_graph = SceneGraph(models_dir, scene_graph_dir, scene_name, accepted_cats)
+        # add node attributes
         scene_graph.build_from_matterport(scene_name, metadata_path)
-
+        # filter the graph to only contain the objects with accepted category
+        scene_graph.filter_by_accepted_cats()
+        if len(scene_graph.graph) < 2:
+            print('Skipped scene {} as there is 1 or 0 object there ... '.format(scene_name))
+            continue
         # derive scene graphs and save them
         scene_graph.build_scene_graph(test_objects, num_samples=num_samples, chunk_size=500, dist_eps=dist_eps,
                                       angle_eps=angle_eps, room_cats=['wall', 'unknown', 'roof', 'ceiling'],
                                       bad_object_cats=['remove', 'delete', 'void'])
-        if len(scene_graph.graph) > 0:
-            scene_graph.to_json()
+        scene_graph.to_json()
 
         duration_house = (time() - t2) / 60
         print('Scene {} took {} minutes to process'.format(scene_name, round(duration_house, 2)))
@@ -49,6 +53,7 @@ def main(num_chunks, chunk_idx, action='build_scene_graphs'):
     test_objects = []
     models_dir = '../../data/matterport3d/models'
     metadata_path = '../../data/matterport3d/metadata.csv'
+    accepted_cats = set(load_from_json('../../data/matterport3d/accepted_cats.json'))
     scene_graph_dir = '../../results/matterport3d/GKRank/scene_graphs/all'
     if not os.path.exists(scene_graph_dir):
         os.makedirs(scene_graph_dir)
@@ -59,6 +64,7 @@ def main(num_chunks, chunk_idx, action='build_scene_graphs'):
         process_scenes(scene_files[chunk_idx * chunk_size: (chunk_idx + 1) * chunk_size],
                        models_dir,
                        metadata_path,
+                       accepted_cats,
                        scene_graph_dir,
                        test_objects=test_objects,
                        num_samples=1000,
