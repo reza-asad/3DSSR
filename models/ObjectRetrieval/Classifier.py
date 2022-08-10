e0e52245a29ac94b88162345cb00945e0141e3df
rasad <rayza.asad@gmail.com>
2022-08-09 19:09:04 -0700

    adding region classification

--- models/ObjectRetrieval/Classifier.py
+++ models/ObjectRetrieval/Classifier.py
@@ -7,55 +7,14 @@ from torch.utils.data import DataLoader
 import pandas as pd
 import numpy as np
 
-from subscene_dataset import SubScene
-from models.LearningBased.transformer_models import PointTransformerSeg
-from models.LearningBased.projection_models import DINOHead
+from region_dataset import Region
+from transformer_models import PointTransformerSeg
+from projection_models import DINOHead
 from scripts.helper import load_from_json
-import models.LearningBased.utils as utils
+import utils as utils
 
 alpha = 1
 gamma = 2.
-
-
-class Backbone(nn.Module):
-    def __init__(self, shape_backbone, layout_backbone, num_point=4096, num_objects=5):
-        super().__init__()
-        self.shape_backbone = shape_backbone
-        self.layout_backbone = layout_backbone
-        self.num_point = num_point
-        self.num_objects = num_objects
-
-    def forward(self, x, centroid_x):
-        # apply shape backbone.
-        x = x.reshape(-1, self.num_point, 3)
-        x = self.shape_backbone(x)
-        x = x.mean(1)
-
-        # add positional encoding to the codes.
-        centroid_x = centroid_x.reshape(-1, 3)
-        x = torch.cat([centroid_x, x], dim=1).unsqueeze(dim=0)
-
-        # apply the layout backbone to x.
-        batch_size, num_curr_objects, input_dim = x.shape
-        if num_curr_objects < self.num_objects:
-            x = x.reshape(-1, num_curr_objects, input_dim)
-        else:
-            x = x.reshape(-1, self.num_objects, input_dim)
-        x = self.layout_backbone(x).squeeze(dim=0)
-
-        return x
-
-
-class Classifier(nn.Module):
-    def __init__(self, backbone, head):
-        super(Classifier, self).__init__()
-        self.backbone = backbone
-        self.head = head
-
-    def forward(self, x, centroid_x):
-        output = self.backbone(x, centroid_x)
-
-        return self.head(output)
 
 
 def compute_accuracy(per_class_accuracy, predicted_labels, labels, cat_to_idx):
@@ -94,95 +53,54 @@ def evaluate_net(classifier, valid_loader, cat_to_idx):
     with torch.no_grad():
         for i, data in enumerate(valid_loader):
             # load data
-            anchor_idx = data['anchor_idx'].squeeze(dim=0)[0]
-            pc = data['pc'].squeeze(dim=0)
-            centroid = data['centroid'].squeeze(dim=0)
-            label = data['label'].squeeze(dim=0)
+            file_names = data['file_name']
+            pc = data['crops'][0]
+            labels = data['labels'].squeeze(dim=1)
 
             # move data to the right device
             pc = pc.to(dtype=torch.float32).cuda()
-            centroid = centroid.to(dtype=torch.float32).cuda()
-            label = label.to(dtype=torch.long).cuda()
+            labels = labels.to(dtype=torch.long).cuda()
 
             # apply the model
-            output = classifier(pc, centroid)[anchor_idx:anchor_idx+1, :]
-            label = label[anchor_idx:anchor_idx+1]
+            output = classifier(pc)
 
             # compute loss
-            ce_loss = torch.nn.functional.cross_entropy(output, label, reduction='none')
+            ce_loss = torch.nn.functional.cross_entropy(output, labels, reduction='none')
             pt = torch.exp(-ce_loss)
             loss = (alpha * (1 - pt) ** gamma * ce_loss).mean()
             total_validation_loss += loss.item()
 
             _, predictions = torch.max(torch.softmax(output, dim=1), dim=1)
-            compute_accuracy(per_class_accuracy, predictions, label, cat_to_idx)
+            compute_accuracy(per_class_accuracy, predictions, labels, cat_to_idx)
+
+            # map file_names to predicted labels.
+            for j, file_name in enumerate(file_names):
+                file_name = int(file_name.item())
+                predicted_labels[file_name] = predictions[j].item()
 
     return total_validation_loss/(i + 1), per_class_accuracy, predicted_labels
 
 
 def train_net(args):
     # create the training dataset
-    train_dataset = SubScene(scene_dir=args.scene_dir, pc_dir=args.pc_dir, metadata_path=args.metadata_path,
-                             accepted_cats_path=args.accepted_cats_path, max_coord_box=args.max_coord_box,
-                             max_coord_scene=args.max_coord_scene, num_points=args.num_point,
-                             num_objects=args.num_objects, mode='train', file_name_to_idx=args.file_name_to_idx,
-                             with_transform=args.with_transform, random_subscene=args.random_subscene,
-                             batch_one=args.batch_one, global_frame=args.global_frame)
-
-    val_dataset = SubScene(scene_dir=args.scene_dir, pc_dir=args.pc_dir, metadata_path=args.metadata_path,
-                           accepted_cats_path=args.accepted_cats_path, max_coord_box=args.max_coord_box,
-                           max_coord_scene=args.max_coord_scene, num_points=args.num_point,
-                           num_objects=args.num_objects, mode='val', file_name_to_idx=args.file_name_to_idx,
-                           with_transform=False, random_subscene=args.random_subscene, batch_one=True,
-                           global_frame=args.global_frame)
+    train_dataset = Region(args.pc_dir, args.scene_dir, args.metadata_path, max_coord=args.max_coord,
+                           num_local_crops=0, num_global_crops=0, mode='train', cat_to_idx=args.cat_to_idx,
+                           num_points=args.num_point, file_name_to_idx=args.file_name_to_idx)
+    val_dataset = Region(args.pc_dir, args.scene_dir, args.metadata_path, max_coord=args.max_coord,
+                         num_local_crops=0, num_global_crops=0, mode='val', cat_to_idx=args.cat_to_idx,
+                         num_points=args.num_point, file_name_to_idx=args.file_name_to_idx)
     print('{} train point clouds'.format(len(train_dataset)))
     print('{} val point clouds'.format(len(val_dataset)))
 
     # create the dataloaders
     train_loader = DataLoader(train_dataset, batch_size=args.batch_size, num_workers=args.num_workers, shuffle=True)
-    val_loader = DataLoader(val_dataset, batch_size=1, num_workers=args.num_workers, shuffle=False)
+    val_loader = DataLoader(val_dataset, batch_size=args.batch_size, num_workers=args.num_workers, shuffle=False)
 
     # load the backbone from the pretrained classifier
-    shape_backbone = PointTransformerSeg(args)
+    backbone = PointTransformerSeg(args)
     head = DINOHead(in_dim=args.transformer_dim, out_dim=args.num_class, use_bn=args.use_bn_in_head,
                     norm_last_layer=args.norm_last_layer)
-    classifier = utils.DINO(shape_backbone, head, num_local_crops=0, num_global_crops=1, network_type='teacher')
-    classifier = torch.nn.DataParallel(classifier).cuda()
-    if args.pre_training:
-        checkpoint_path = os.path.join(args.cp_dir_pret, args.results_folder_name_pret, args.pre_training_checkpoint)
-        checkpoint = torch.load(checkpoint_path)
-        classifier.load_state_dict(checkpoint['model_state_dict'])
-        shape_backbone = classifier.module.backbone
-    else:
-        # no pretraining.
-        shape_backbone = classifier.module.backbone
-
-    if args.freeze_shape_backbone:
-        shape_backbone.requires_grad_(False)
-    else:
-        shape_backbone.requires_grad_(True)
-
-    # arguments for the layout matching model.
-    layout_args = argparse.Namespace()
-    exceptions = {'input_dim': 35, 'nblocks': 0}
-    for k, v in vars(args).items():
-        if k in exceptions:
-            vars(layout_args)[k] = exceptions[k]
-        else:
-            vars(layout_args)[k] = v
-
-    # build layout matching backbone with learned weights.
-    layout_backbone = PointTransformerSeg(layout_args)
-    layout_backbone.requires_grad_(True)
-
-    # combine the backbones and build a classifier.
-    backbone = Backbone(shape_backbone=shape_backbone, layout_backbone=layout_backbone, num_point=args.num_point,
-                        num_objects=args.num_objects)
-    head = DINOHead(in_dim=args.transformer_dim, out_dim=args.num_class, use_bn=args.use_bn_in_head,
-                    norm_last_layer=args.norm_last_layer)
-    # learn weights for the classifier.
-    head.requires_grad_(True)
-    classifier = Classifier(backbone=backbone, head=head)
+    classifier = utils.DINO(backbone, head, num_local_crops=0, num_global_crops=1, network_type='teacher')
     classifier = torch.nn.DataParallel(classifier).cuda()
     classifier.train()
 
@@ -226,22 +144,18 @@ def train_net(args):
 
         for i, data in enumerate(train_loader):
             # load data
-            pc = data['pc']
-            centroid = data['centroid']
-            label = data['label']
+            pc = data['crops'][0]
+            labels = data['labels'].squeeze(dim=1)
 
             # move data to the right device
             pc = pc.to(dtype=torch.float32).cuda()
-            centroid = centroid.to(dtype=torch.float32).cuda()
-            label = label.to(dtype=torch.long).cuda()
+            labels = labels.to(dtype=torch.long).cuda()
 
             # apply the model
-            output = classifier(pc, centroid)
-            output = output.reshape(-1, args.num_class)
-            label = label.reshape(-1)
+            output = classifier(pc)
 
             # compute loss
-            ce_loss = torch.nn.functional.cross_entropy(output, label, reduction='none')
+            ce_loss = torch.nn.functional.cross_entropy(output, labels, reduction='none')
             pt = torch.exp(-ce_loss)
             loss = (alpha * (1 - pt) ** gamma * ce_loss).mean()
 
@@ -302,6 +216,7 @@ def train_net(args):
         print('-' * 50)
 
     # report the attributes for the best model
+    print('Best mode is from epoch: {}'.format(best_epoch))
     print('Best Validation loss is {}'.format(best_validation_loss))
     print('Best accuracy on validation: ')
     compute_accuracy_stats(best_accuracy)
@@ -314,9 +229,9 @@ def get_args():
     parser.add_argument('--accepted_cats_path', default='../../data/{}/accepted_cats.json')
     parser.add_argument('--metadata_path',  default='../../data/{}/metadata.csv')
     parser.add_argument('--pc_dir', default='../../data/{}/pc_regions')
-    parser.add_argument('--scene_dir', default='../../results/{}/scenes')
+    parser.add_argument('--scene_dir', default='../../data/{}/scenes')
     parser.add_argument('--cp_dir', default='../../results/{}/ObjectRetrieval/')
-    parser.add_argument('--results_folder_name', default='region_classification_transformer_full')
+    parser.add_argument('--results_folder_name', default='region_classification_full')
     parser.add_argument('--checkpoint', default='checkpoint.pth')
 
     # Model
@@ -325,24 +240,16 @@ def get_args():
     parser.add_argument('--nneighbor', default=16, type=int)
     parser.add_argument('--input_dim', default=3, type=int)
     parser.add_argument('--transformer_dim', default=32, type=int)
-    parser.add_argument('--max_coord_box', default=3.65, type=float, help='3.65 for MP3D')
-    parser.add_argument('--max_coord_scene', default=13.07, type=float, help='13.07 for MP3D')
+    parser.add_argument('--max_coord', default=3.65, type=float, help='3.65 for MP3D')
     parser.add_argument('--use_bn_in_head', default=False, type=utils.bool_flag)
     parser.add_argument('--norm_last_layer', default=True, type=utils.bool_flag)
 
     # Optim
-    parser.add_argument('--epochs', default=100, type=int, help='number of epochs')
+    parser.add_argument('--epochs', default=50, type=int, help='number of epochs')
     parser.add_argument('--lr', default=1e-4, type=float)
     parser.add_argument('--weight_decay', default=1e-4, type=float)
-    parser.add_argument('--batch_size', default=2, type=int)
-    parser.add_argument('--num_objects', default=5, type=int)
+    parser.add_argument('--batch_size', default=8, type=int)
     parser.add_argument('--num_workers', default=4, type=int)
-    parser.add_argument('--pre_training', default=True, type=utils.bool_flag)
-    parser.add_argument('--with_transform', default=True, type=utils.bool_flag)
-    parser.add_argument('--random_subscene', default=False, type=utils.bool_flag)
-    parser.add_argument('--freeze_shape_backbone', default=True, type=utils.bool_flag)
-    parser.add_argument('--global_frame', default=True, type=utils.bool_flag)
-    parser.add_argument('--pre_training_checkpoint', default='CP_best.pth')
     parser.add_argument('--save_cp', action='store_true', default=True, help='save the trained models')
 
     return parser
