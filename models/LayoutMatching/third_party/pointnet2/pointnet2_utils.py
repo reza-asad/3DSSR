f45fe4d0bf74cd0704683974f713cd78fb70977e
rasad <rayza.asad@gmail.com>
2022-09-04 14:00:59 -0700

    adding equivariant point features.

--- models/LayoutMatching/third_party/pointnet2/pointnet2_utils.py
+++ models/LayoutMatching/third_party/pointnet2/pointnet2_utils.py
@@ -12,12 +12,13 @@ import torch
 from torch.autograd import Function
 import torch.nn as nn
 import pytorch_utils as pt_utils
+import pytorch3d.ops as pt3d_ops
 import sys
 
-# try:
-import builtins
-# except:
-#     import __builtin__ as builtins
+try:
+    import builtins
+except:
+    import __builtin__ as builtins
 
 try:
     import pointnet2._ext as _ext
@@ -420,3 +421,75 @@ class GroupAll(nn.Module):
             return new_features, grouped_xyz
         else:
             return new_features
+
+# TODO: add equivariant query and group.
+class QueryAndGroupEquiv(nn.Module):
+    r"""
+    Groups with a ball query of radius
+    Parameters
+    ---------
+    radius : float32
+        Radius of ball
+    nsample : int32
+        Maximum number of features to gather in the ball
+    """
+
+    def __init__(self, radius, nsample, use_xyz=True):
+        super(QueryAndGroupEquiv, self).__init__()
+        self.radius, self.nsample, self.use_xyz = radius, nsample, use_xyz
+
+    def forward(self, xyz, new_xyz, features=None):
+        r"""
+        Parameters
+        ----------
+        xyz : torch.Tensor
+            xyz coordinates of the features (B, N, 3)
+        new_xyz : torch.Tensor
+            centriods (B, Np, 3)
+        features : torch.Tensor
+            (B, C, Nr, N) where Nr is #rot, N is #input_pts
+        Returns
+        -------
+        new_features : torch.Tensor
+            (B, 3 + C, Nr, Np, Nnb) tensor
+        grouped_xyz: (B,3,Np,Nnb)
+        bq_idx: [B, Np, Nnb] ranging {0, 1, ..., Np-1}
+        """
+        idx = ball_query(self.radius, self.nsample, xyz, new_xyz).long()  # [B, N_pts, N_nb]
+        bq_idx = idx.clone()
+
+        # create shadow feature idx
+        N = xyz.shape[1]
+        idx_ = idx.permute([2, 0, 1])  # [N_nb, B, N_pts]
+        first_idxs = idx_[0]  # [B, N_pts]
+        idx_zeroed = idx_ - first_idxs
+        duplica_ind = idx_zeroed == 0
+        duplica_ind[0] = False
+        duplica_ind_ = duplica_ind.permute([1, 2, 0])
+        idx[duplica_ind_] = N  # [B, Np, Nnb]
+
+        shadow_xyz = 1e6 * torch.ones_like(xyz[:, 0:1, :])
+        xyz = torch.cat([xyz, shadow_xyz], dim=1)  # (B,N+1,3)
+        grouped_xyz = pt3d_ops.knn_gather(xyz, idx)  # [B,Np,Nnb,3]
+        grouped_xyz = grouped_xyz.permute([0, 3, 1, 2])  # [B,3,Np,Nnb]
+        grouped_xyz -= new_xyz.transpose(1, 2).unsqueeze(-1)
+
+        shadow_features = torch.zeros_like(features[..., 0:1])
+        features = torch.cat([features, shadow_features], dim=-1)  # (B,C,Nr,N+1)
+        B, C, Nr, N1 = features.shape
+        _, Np, Nnb = idx.shape
+        features = features.flatten(start_dim=1, end_dim=2)  # [B,C*Nr,N+1]
+        grouped_features = pt3d_ops.knn_gather(features.permute([0, 2, 1]), idx)  # [B,Np, Nnb, C*Nr]
+        grouped_features = grouped_features.permute([0, 3, 1, 2])  # [B,C*Nr,Np,Nnb]
+        grouped_features = grouped_features.view([B, C, Nr, Np, Nnb])
+        if self.use_xyz:
+            xyz_feature = grouped_xyz.clone()
+            xyz_feature[xyz_feature > 1e5] = 0
+            xyz_feature = xyz_feature / self.radius  # [B,3,Np,Nnb]
+            new_features = torch.cat(
+                [xyz_feature[:,:,None,...].expand([-1,-1,Nr,-1,-1]), grouped_features], dim=1  # the filter need not to know abs pts scale because that info has been provided by its layer index
+            )  # (B, 3+C, Nr,Np,Nnb)
+        else:
+            new_features = grouped_features
+
+        return new_features, grouped_xyz, bq_idx
